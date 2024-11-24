2dd1259473bb44d5366aef662f84deabb2b27f3c
ILoveRIRIKA <764977661@qq.com>
2024-11-24 16:23:36 +0800

    装配bean项目spring-ioc-appcontext, 更新为连接DB版本

--- Project/Spring Project/spring-ioc-appcontext/src/main/java/com/ILoveWsq/service/UserService.java
+++ Project/Spring Project/spring-ioc-appcontext/src/main/java/com/ILoveWsq/service/UserService.java
@@ -1,48 +1,108 @@
 package com.ILoveWsq.service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Objects;
+
 
 public class UserService {
     private MailService mailService;
+
+    private DataSource dataSource;
 
     public void setMailService(MailService mailService) {
         this.mailService = mailService;
     }
 
-    //存放所有用户
-    private List<User> users = new ArrayList<>(Arrays.asList(
-            new User(1L, "bob@example.com", "password", "2Bob"),
-            new User(2L, "alice@example.com", "password", "Alice"),
-            new User(3L, "tom@example.com", "password", "Tom")));
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+//    //存放所有用户
+//    private List<User> users = new ArrayList<>(Arrays.asList(
+//            new User(1L, "bob@example.com", "password", "2Bob"),
+//            new User(2L, "alice@example.com", "password", "Alice"),
+//            new User(3L, "tom@example.com", "password", "Tom")));
 
     public User login(String email, String password) {
-        for (User user : users) {
-            if (user.getEmail().equalsIgnoreCase(email) && user.getPassword().equals(password)) {
-                mailService.sendLoginMessage(user);
-                return user;
+        try (Connection conn = dataSource.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement("SELECT id,email,password,name FROM user WHERE email=? AND password=?")) {
+                ps.setObject(1, email);
+                ps.setObject(2, password);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        User user = new User(rs.getLong(1), rs.getString(2), rs.getString(3), rs.getString(4));
+                        mailService.sendLoginMessage(user);
+                        return user;
+                    }
+                }
             }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
+
         throw new RuntimeException("login failed");
     }
 
     public User getUser(long id) {
-        return users.stream().filter(user -> user.getId() == id).findFirst().orElse(null);
+        try (Connection conn = dataSource.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement("SELECT id,email,password,name FROM user WHERE id=?")) {
+                ps.setObject(1, id);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        User user = new User(rs.getLong(1), rs.getString(2), rs.getString(3), rs.getString(4));
+                        return user;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public User register(String email, String password, String name) {
-        users.forEach(user -> {
-            if (user.getEmail().equalsIgnoreCase(email)) {
-                throw new RuntimeException("email existed");
+        Long id = null;
+        User user = null;
+        try (Connection conn = dataSource.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement("SELECT id,email,password,name FROM user WHERE email=?")) {
+                ps.setObject(1, email);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        throw new RuntimeException("email existed");
+                    }
+                }
             }
-        });
 
-        User user = new User(users.stream().mapToLong(u -> u.getId()).max().getAsLong() + 1, email, password, name);
-        users.add(user);
-        mailService.sendRegistrationMessage(user);
-        return user;
+
+            try (PreparedStatement ps = conn.prepareStatement("SELECT id FROM user ORDER BY id DESC LIMIT 1")) {
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        id = rs.getLong(1) + 1;
+                    }
+                }
+            }
+
+            if (Objects.nonNull(id)) {
+                user = new User(id, email, password, name);
+            }
+
+            try (PreparedStatement ps = conn.prepareStatement("INSERT INTO user (id, email, password, name) VALUES (?, ?, ?, ?)")) {
+                if (Objects.isNull(user)) {
+                    return null;
+                }
+                ps.setObject(1, user.getId());
+                ps.setObject(2, user.getEmail());
+                ps.setObject(3, user.getPassword());
+                ps.setObject(4, user.getName());
+                ps.executeUpdate();
+                mailService.sendRegistrationMessage(user);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
-
 }
