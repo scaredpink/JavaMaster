8db110e4455092d4822938293c08834498d9eec8
baitao05 <baitao05@meituan.com>
2024-12-02 20:04:00 +0800

    2024-12-02

---
+++ Project/JavaEE Project/web-servlet-hello/src/main/java/com/i/love/wsq/HelloServlet.java
@@ -0,0 +1,29 @@
+package com.i.love.wsq;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author baitao05
+ */
+// WebServlet注解表示这是一个Servlet，并映射到地址/:
+@WebServlet(urlPatterns = "/")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        // 设置响应类型:
+        resp.setContentType("text/html");
+        // 获取输出流:
+        PrintWriter pw = resp.getWriter();
+        // 写入响应:
+        pw.write("<h1>Hello, world!" + name + "</h1>");
+        // 最后不要忘记flush强制输出:
+        pw.flush();
+    }
+}
