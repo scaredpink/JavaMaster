bdb6106319c26c6e49f7be561128bc358fa57fc5
Colourful White <scaredpink@163.com>
2025-02-19 00:32:20 +0800

    2025-02-18 night

---
+++ Codeforces/Codeforces Round 1003 (Div. 4)/A.java
@@ -0,0 +1,10 @@
+public class A {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        for (int i = 0; i < t; i ++ ) {
+            String str = scan.next();
+            
+        }
+    }
+}
--- Project/JavaSE Project/java-se/java-claseese-autocloseable/src/main/java/i/love/wsq/Main.java
+++ Project/JavaSE Project/java-se/java-claseese-autocloseable/src/main/java/i/love/wsq/Main.java
@@ -2,6 +2,8 @@ package i.love.wsq;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String a = "I";
+        a = a +  " Love ";
+        a += "Wsq!";
     }
 }
