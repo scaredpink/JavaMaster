ecfa585bf566a69351df459ec2aa33ee60120694
Colourful White <scaredpink@163.com>
2025-02-21 00:02:50 +0800

    2025-02-20 night

--- Codeforces/Codeforces Round 1003 (Div. 4)/A.java
+++ Codeforces/Codeforces Round 1003 (Div. 4)/A.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 public class A {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
         for (int i = 0; i < t; i ++ ) {
             String str = scan.next();
-            
+            int index = str.lastIndexOf("us");
+            System.out.println(str.substring(0, index) + "i");
         }
     }
 }
--- Codeforces/Codeforces Round 1003 (Div. 4)/B.java
+++ Codeforces/Codeforces Round 1003 (Div. 4)/B.java
@@ -1,11 +1,9 @@
-package i.love.wsq;
-
 import java.util.Scanner;
 
-public class Main {
+public class B {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
+        int n = scan.nextInt(); 
         for (int i = 0; i < n; i ++ ) {
             String str = scan.next();
             if (equalNextChar(str) != -1) {
