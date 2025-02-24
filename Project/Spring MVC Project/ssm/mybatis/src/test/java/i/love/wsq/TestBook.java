7969f80ffbdec4ac5602a25e3a9a5ff81c362111
ILoveRIRIKA <764977661@qq.com>
2025-02-24 21:14:25 +0800

    2025-02-24 night

--- Project/Spring MVC Project/ssm/mybatis/src/test/java/i/love/wsq/TestBook.java
+++ Project/Spring MVC Project/ssm/mybatis/src/test/java/i/love/wsq/TestBook.java
@@ -3,29 +3,75 @@ package i.love.wsq;
 import i.love.wsq.domain.Book;
 import i.love.wsq.util.MyBatisSessionFactory;
 import java.io.InputStream;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.*;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.ibatis.io.Resources;
 
-/**
- * @author baitao05
- */
 public class TestBook {
     private static final Logger LOGGER = LoggerFactory.getLogger(TestBook.class);
 
     @Test
     public void testDoCreate() throws Exception {
-        InputStream input = Resources.getResourceAsStream("mybatis/mybatis.cfg.xml");
-        SqlSession session = MyBatisSessionFactory.getSession();
-        Book book = new Book("红高粱", "莫言", 66.66);
-        LOGGER.info("doCreate {}", session.insert("i.love.wsq.BookNS.doCreate", book));
+        Book book = new Book("活着", "余华", 55.55);
+        LOGGER.info("doCreate {}", MyBatisSessionFactory.getSession().insert("i.love.wsq.BookNS.doCreate", book));
         LOGGER.info("插入数据 {}", book);
-        session.commit();
-        session.close();
-        input.close();
+        MyBatisSessionFactory.getSession().commit();
+        MyBatisSessionFactory.close();
+    }
+
+    @Test
+    public void testFindById() throws Exception {
+        Long id = 1L;
+        Book book = MyBatisSessionFactory.getSession().selectOne("i.love.wsq.BookNS.findById", id);
+        LOGGER.info("findById {}", book);
+        MyBatisSessionFactory.close();
+    }
+
+    @Test
+    public void testFindAll() throws Exception {
+        List<Book> bookList = MyBatisSessionFactory.getSession().selectList("i.love.wsq.BookNS.findAll");
+        LOGGER.info("findById {}", bookList);
+        MyBatisSessionFactory.close();
+    }
+
+    @Test
+    public void testFindSplit() throws Exception {
+        Map<String, Object> splitParams = new HashMap<>();
+        splitParams.put("column", "name");
+        splitParams.put("keyword", "%%");
+        splitParams.put("start", 0);
+        splitParams.put("lineSize", 2);
+        List<Book> bookList = MyBatisSessionFactory.getSession().selectList("i.love.wsq.BookNS.findSplit", splitParams);
+        LOGGER.info("findSplit {}", bookList);
+        MyBatisSessionFactory.close();
+    }
+
+    @Test
+    public void testGetAllCount() throws Exception {
+        Map<String, Object> splitParams = new HashMap<>();
+        splitParams.put("column", "name");
+        splitParams.put("keyword", "%%");
+        Long count = MyBatisSessionFactory.getSession().selectOne("i.love.wsq.BookNS.getAllCount", splitParams);
+        LOGGER.info("findAllCount {}", count);
+        MyBatisSessionFactory.getSession().close();
+    }
+
+    @Test
+    public void testResultHandler() throws Exception {
+        Map<Long, String> bookMap = new HashMap<>();
+        MyBatisSessionFactory.getSession().select("i.love.wsq.BookNS.findAll", new ResultHandler() {
+            @Override
+            public void handleResult(ResultContext resultContext) {
+                Book book = (Book) resultContext.getResultObject();
+                bookMap.put(book.getBid(), book.getAuthor() + ": " + book.getName());
+            }
+        });
+        LOGGER.info("ResultHandler: {}", bookMap);
     }
 }
